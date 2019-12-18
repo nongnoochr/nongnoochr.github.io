32420ac2a9dbc0d44ba21d8610a30d39a3ac842f
Nongnooch Roongpiboonsopit <nongnoochr@gmail.com>
2019-12-18 17:08:10 +0700

    Test

---
+++ precache-manifest.470cb9857a75dec0cf98b186ece69985.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08da9383d9a601c2c547cf61b51df99b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fab2a8b0dd1cfc177814",
+    "url": "/static/css/main.3365de13.chunk.css"
+  },
+  {
+    "revision": "900ac157f6c6fe92e46a",
+    "url": "/static/js/2.04a4c065.chunk.js"
+  },
+  {
+    "revision": "fab2a8b0dd1cfc177814",
+    "url": "/static/js/main.29fa1e85.chunk.js"
+  },
+  {
+    "revision": "ffc7783e42c3dc087897",
+    "url": "/static/js/runtime-main.586eb4c0.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
