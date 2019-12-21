4d68e5d28bbb690036b5e8cb815a0a6b6a7ea4b8
Nongnooch Roongpiboonsopit <nongnoochr@gmail.com>
2019-12-22 00:39:33 +0700

    Temp project

---
+++ precache-manifest.1a78dc733580af2c78d04f10f37be1aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df7d31ba7dfc44c7187c4104b2a7ec3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66b4911d960febd2d067",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "081af5a134596ab0c4a6",
+    "url": "/static/css/main.ade39830.chunk.css"
+  },
+  {
+    "revision": "66b4911d960febd2d067",
+    "url": "/static/js/2.283856d6.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.283856d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "081af5a134596ab0c4a6",
+    "url": "/static/js/main.893eebbc.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
